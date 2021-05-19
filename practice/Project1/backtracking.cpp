55e3d46dccb507e57ba52e488d46cb01bee89641
eunsol <a2670624@naver.com>
2021-05-19 17:52:23 +0900

    5/19 dp & backtracking

--- practice/Project1/backtracking.cpp
+++ practice/Project1/backtracking.cpp
@@ -1,17 +1,4 @@
-//boj15649 N과 M(1)
-//boj15650 N과 M(2)
-//boj15651 N과 M(3)
-//boj15652 N과 M(4)
-//boj15654 N과 M(5)
-//boj15655 N과 M(6) (next_permutation)
-//boj12101 1,2,3 더하기 2
-//boj9663 N-Queen
-//boj1182 부분수열의 합
-// (다른풀이)
-// 각 수를 더할지 말지를 고려하며 모든 부분수열의 합 상태공간 트리를 그린다고 생각
-// 시간 복잡도는 O(n^2) 
-
-//next_permutation
+////next_permutation
 //#include<iostream>
 //#include<algorithm>
 //using namespace std;
@@ -50,3 +37,17 @@
 //
 //	return 0;
 //}
+
+//boj15649 N과 M(1)
+//boj15650 N과 M(2)
+//boj15651 N과 M(3)
+//boj15652 N과 M(4)
+//boj15654 N과 M(5)
+//boj15655 N과 M(6) (next_permutation)
+//boj12101 1,2,3 더하기 2
+//boj9663 N-Queen
+//boj1182 부분수열의 합
+// (다른풀이)
+// 각 수를 더할지 말지를 고려하며 모든 부분수열의 합 상태공간 트리를 그린다고 생각
+// 시간 복잡도는 O(n^2) 
+//boj18428 감시 피하기
